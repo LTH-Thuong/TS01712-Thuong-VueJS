91dfee728ea3d3442c0d3d334af3966389d0ca9f
Lê Thị Hoài Thương <thuonglthts01712@gmail.com>
2026-01-30 23:00:49 +0700

    lab 6

---
+++ Lab6/src/components/bai1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Nhập điểm trung bình của học sinh</h3>
+  </div>
+  <input type="number" step="0,1" v-model.number="dtb" />
+  <p v-if="dtb < 0 || dtb > 10" style="color : red">
+    Vui lòng nhập điểm từ 0 -> 10!
+  </p>
+  <p v-else-if="dtb < 5.0">Xếp loại: Yếu</p>
+  <p v-else-if="dtb < 6.5">Xếp loại: Trung bình</p>
+  <p v-else-if="dtb < 8.0">Xếp loại: Khá</p>
+  <p v-else-if="dtb < 9.0">Xếp loại: Giỏi</p>
+  <p v-else>Xếp loại: Xuất xắc</p>
+</template>
+
+<script setup>
+  import{ref} from 'vue'
+  const dtb = ref(0);
+</script>
---
+++ Lab6/src/components/bai2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>
+      Nhập tháng của bạn
+    </h3>
+    <input type="number" v-model.number="month"/>
+    <p v-if="month < 1 || month > 12">Vui lòng nhập tháng vào đây</p>
+    <p v-else-if="month >= 1 && month <= 3">Mùa Xuân</p>
+    <p v-else-if=" month <= 6">Mùa Hè</p>
+    <p v-else-if="month <= 9">Mùa Thu</p>
+    <p v-else>Mùa Đông</p>
+  </div>
+</template>
+<script setup>
+  import{ref} from 'vue';
+  const month = ref(1);
+</script>
